09bd13e2acb35a76826d8736ef91b17b2fd4ab2c
“x-raf <x-raf@wp.pl>
2018-05-11 18:11:27 +0200

    Refactor

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -6,26 +6,5 @@ import com.kodilla.testing.user.SimpleUser;
 public class TestingMain {
     public static void main(String[] args){
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
-
-        //SimpleUser
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")){
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        // Testing Calculator class
-        Calculator calculator = new Calculator();
-        int addResult = calculator.add(123, 123);
-        int subResult = calculator.substract(123, 122);
-        if (addResult == 246 && subResult == 1) {
-            System.out.println("Calculator test OK");
-        } else {
-            System.out.println("Calculator has error!");
-        }
     }
 }
