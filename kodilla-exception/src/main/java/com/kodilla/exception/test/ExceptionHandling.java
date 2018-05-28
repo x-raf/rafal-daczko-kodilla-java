4f89009fbcc888a2466f69d5593f528e94feb914
“x-raf <x-raf@wp.pl>
2018-05-28 06:17:03 +0200

    Submodule 8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,4 +1,17 @@
 package com.kodilla.exception.test;
 
 public class ExceptionHandling extends Exception {
+
+    public static void main(String[] args) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+        try {
+            String resultSecondChallenge = secondChallenge.probablyIWillThrowException(3,1.5);
+            System.out.println(resultSecondChallenge);
+        }catch (ExceptionHandling e) {
+            System.out.println("Znaleziono wyjątek: " + e);
+        }finally {
+            System.out.println("Złe wartości zmiennych: x >= 2 || x < 1 || y == 1.5");
+        }
+    }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/SecondChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/SecondChallenge.java
@@ -7,17 +7,4 @@ public class SecondChallenge {
         }
         return "Done!";
     }
-
-    public static void main(String[] args) {
-
-        SecondChallenge secondChallenge = new SecondChallenge();
-        try {
-            String resultSecondChallenge = secondChallenge.probablyIWillThrowException(3,1.5);
-            System.out.println(resultSecondChallenge);
-        }catch (ExceptionHandling e) {
-            System.out.println("Znaleziono wyjątek: " + e);
-        }finally {
-            System.out.println("Złe wartości zmiennych: x >= 2 || x < 1 || y == 1.5");
-        }
-    }
 }
