b5d6d188726704acd5aaf993bae66e10e8953aad
“x-raf <x-raf@wp.pl>
2018-05-30 19:16:58 +0200

    Submodule 9.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -1,4 +1,43 @@
 package com.kodilla.good.patterns.challenges;
 
-public class MovieStore {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class MovieStore {
+
+    public static Map<String, List<String>> getMovies() {
+
+        List<String> ironManTranslations = new ArrayList<>();
+        ironManTranslations.add("Żelazny Człowiek");
+        ironManTranslations.add("Iron Man");
+
+        List<String> avengersTranslations = new ArrayList<>();
+        avengersTranslations.add("Mściciele");
+        avengersTranslations.add("Avengers");
+
+        List<String> flashTranslations = new ArrayList<>();
+        flashTranslations.add("Błyskawica");
+        flashTranslations.add("Flash");
+
+        Map<String, List<String>> booksTitlesWithTranslations = new HashMap<>();
+        booksTitlesWithTranslations.put("IM", ironManTranslations);
+        booksTitlesWithTranslations.put("AV", avengersTranslations);
+        booksTitlesWithTranslations.put("FL", flashTranslations);
+
+        return booksTitlesWithTranslations;
+    }
+    public static String showUpperAndReturn(String s) {
+        System.out.println(s);
+        return s.toUpperCase();
+    }
+    public static void main(String[] args) {
+        String s = getMovies().entrySet().stream()
+                .flatMap(entry -> entry.getValue().stream())
+                .map(MovieStore::showUpperAndReturn)
+                .collect(Collectors.joining("!"));
+        System.out.println(s);
+    }
 }
