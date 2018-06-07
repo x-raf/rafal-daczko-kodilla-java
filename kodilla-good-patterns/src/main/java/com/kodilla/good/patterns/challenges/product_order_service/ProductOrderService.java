da086841dd913c564fc1e68e27b5bc866b2709d8
“x-raf <x-raf@wp.pl>
2018-06-07 18:38:37 +0200

    Submodule 9.2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/product_order_service/ProductOrderService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/product_order_service/ProductOrderService.java
@@ -3,7 +3,7 @@ package com.kodilla.good.patterns.challenges.product_order_service;
 public class ProductOrderService {
     public static void main(String[] args) {
         OrderDetailsRetriever orderDetailsRetriever = new OrderDetailsRetriever();
-        OrderDetails orderDetails = new OrderDetailsRetriever().retrieve();
+        OrderDetails orderDetails = orderDetailsRetriever.retrieve();
 
         OrderProcessor orderProcessor = new OrderProcessor(new MailService(), new OrderCreator(), new OrderRetriever());
         orderProcessor.process(orderDetails);
