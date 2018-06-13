792d74ea4e46080ceb0e0ad5c6a21da979b9e4c0
“x-raf <x-raf@wp.pl>
2018-06-13 19:10:34 +0200

    Submodule 7.7

--- kodilla-rps/src/main/java/com/kodilla/rps/Game.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/Game.java
@@ -2,9 +2,11 @@ package com.kodilla.rps;
 
 public class Game {
 
-
+    private int playerPoints;
+    private int computerPoints;
     private UserDialogs userDialogs = new UserDialogs();
     private String playerName = userDialogs.getUserName();
+    private int round;
 
     public void getWinner(int numberOfRounds) {
 
@@ -25,28 +27,44 @@ public class Game {
                 if (computerChoice == 2) {
                     System.out.println(playerName + " choose Rock; Computer choose Paper.");
                     System.out.println("Computer wins!");
+                    computerPoints++;
                 } else {
                     System.out.println(playerName + " choose Rock; Computer choose Scissors.");
                     System.out.println(playerName + " wins!");
+                    playerPoints++;
                 }
             } else if (userChoice == 2) {
                 if (computerChoice == 1) {
                     System.out.println(playerName + " choose Paper; Computer choose Rock.");
                     System.out.println(playerName + " wins!");
+                    playerPoints++;
                 } else {
                     System.out.println(playerName + " choose Paper; Computer choose Scissors.");
                     System.out.println("Computer wins!");
+                    computerPoints++;
                 }
             } else {
                 if (computerChoice == 1) {
                     System.out.println(playerName + " choose Scissors; Computer choose Rock.");
                     System.out.println("Computer wins!");
+                    computerPoints++;
                 } else {
                     System.out.println(playerName + " choose Scissors; Computer choose Paper.");
                     System.out.println(playerName + " wins!");
+                    playerPoints++;
                 }
             }
+            round++;
+            System.out.println("Round number: " + round);
+            System.out.println(playerName + ": " + playerPoints + " points. Computer: " + computerPoints + " points.");
             numberOfRounds--;
+        }
+        if (playerPoints > computerPoints) {
+            System.out.println(playerName + " wins!");
+        }else if (playerPoints == computerPoints){
+            System.out.println("Tie!");
+        }else {
+            System.out.println("Computer wins!");
         }
     }
 }
