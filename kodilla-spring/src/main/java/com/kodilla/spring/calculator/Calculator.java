8a229ae718f47a3af795718d47521bae4938754a
“x-raf <x-raf@wp.pl>
2018-06-19 10:49:40 +0200

    Submodule 10.2

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -10,18 +10,26 @@ public final class Calculator {
     private Display display;
 
     public double add(double a, double b){
-        return display.displayValue(a+b);
+        double sum = a+b;
+        display.displayValue(sum);
+        return sum;
     }
     public double sub(double a, double b){
-        return display.displayValue(a-b);
+        double diff = a-b;
+        display.displayValue(diff);
+        return diff;
     }
     public double mul(double a, double b){
-        return display.displayValue(a*b);
+        double prod = a*b;
+        display.displayValue(prod);
+        return prod;
     }
     public double div(double a, double b) throws ArithmeticException{
         if (b == 0) {
             throw new ArithmeticException("Illegal argument! You can't divide by 0");
         }
-        return display.displayValue(a/b);
+        double quot = a/b;
+        display.displayValue(quot);
+        return quot;
     }
 }
