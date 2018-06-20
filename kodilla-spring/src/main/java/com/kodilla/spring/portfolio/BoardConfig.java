e4f64c2ebfcc737ff133df03763167f1c8ccce6f
“x-raf <x-raf@wp.pl>
2018-06-20 11:18:16 +0200

    Submodule 10.3

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,5 +1,7 @@
 package com.kodilla.spring.portfolio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -7,9 +9,21 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class BoardConfig {
 
+    @Autowired
+    @Qualifier("toDoList")
+    TaskList toDoList;
+
+    @Autowired
+    @Qualifier("inProgressList")
+    TaskList inProgressList;
+
+    @Autowired
+    @Qualifier("doneList")
+    TaskList doneList;
+
     @Bean
-    public Board board() {
-        return new Board(getToDoList(), getInProgressList(), getDoneList());
+    public Board getBoard() {
+        return new Board(toDoList, inProgressList, doneList);
     }
 
     @Bean(name = "toDoList")
