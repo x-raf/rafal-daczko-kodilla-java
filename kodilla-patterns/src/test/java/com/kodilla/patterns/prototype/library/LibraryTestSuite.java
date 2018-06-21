a7e68bc7cfec200bd0338abeccafecf59c3971fb
“x-raf <x-raf@wp.pl>
2018-06-21 18:34:53 +0200

    Submodule 10.3

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -0,0 +1,4 @@
+package com.kodilla.patterns.prototype.library;
+
+public class LibraryTestSuite {
+}
--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -2,15 +2,23 @@ package com.kodilla.spring.portfolio;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class BoardTestSuite {
+    @Autowired
+    private Board board;
     @Test
     public void testTaskAdd() {
         //Given
-        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
-        Board board = context.getBean(Board.class);
+        //ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+        //Board board = context.getBean(Board.class);
 
         TaskList listToDo = board.getToDoList();
         listToDo.addTaskToList("Visit Henry in hospital.");
