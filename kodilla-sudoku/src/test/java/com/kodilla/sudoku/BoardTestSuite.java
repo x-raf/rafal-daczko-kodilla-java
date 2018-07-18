9d4bf982e1e822b61a6fda664aaeb031e8b18407
“x-raf <x-raf@wp.pl>
2018-07-18 20:50:07 +0200

    Submodule 12.7

--- kodilla-sudoku/src/test/java/com/kodilla/sudoku/BoardTestSuite.java
+++ kodilla-sudoku/src/test/java/com/kodilla/sudoku/BoardTestSuite.java
@@ -29,6 +29,21 @@ public class BoardTestSuite {
         board.setValue(9,2,9);
         board.setValue(9,7,4);
         return board;*/
+
+        /*
+        World hardest sudoku solution
+
+        8 1 2 7 5 3 6 4 9
+        9 4 3 6 8 2 1 7 5
+        6 7 5 4 9 1 2 8 3
+        1 5 4 2 3 7 8 9 6
+        3 6 9 8 4 5 7 2 1
+        2 8 7 1 6 9 5 3 4
+        5 2 1 9 7 4 3 6 8
+        4 3 8 5 2 6 9 1 7
+        7 9 6 3 1 8 4 5 2
+        */
+
        board.setValue(1,2,2);
        board.setValue(1,4,4);
        board.setValue(1,5,5);
