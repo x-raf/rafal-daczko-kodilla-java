9575323dc2332cc5010d193982b4f1a9233a0390
“x-raf <x-raf@wp.pl>
2018-07-18 20:48:04 +0200

    Submodule 12.7

--- kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuSolver.java
+++ kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuSolver.java
@@ -23,10 +23,43 @@ public class SudokuSolver {
                             sudokuBoard.setValue(row, column, sudokuBoard.getPossibilities(row, column).get(0));
                             changed = true;
                         }
+                        if (!changed) {
+                            Integer possibleOnlyHere = isPossibleOnlyHere(row, column, sudokuBoard);
+                            if (possibleOnlyHere > 0) {
+                                sudokuBoard.setValue(row, column, possibleOnlyHere);
+                                changed = true;
+                            }
+                        }
                     }
                 }
             }
         }
+    }
+
+    private Integer isPossibleOnlyHere(int row, int column, SudokuBoard sudokuBoard) {
+        for (Integer possibility:sudokuBoard.getPossibilities(row, column)) {
+            boolean result = true;
+            for (int n=BOARD_START_INDEX; n<BOARD_SIZE; n++) {
+                Integer val = sudokuBoard.getValue(row,n);
+                if(val == -1 && sudokuBoard.getPossibilities(row,n).contains(possibility)) result = false;
+            }
+            for (int n=BOARD_START_INDEX; n<BOARD_SIZE; n++) {
+                Integer val = sudokuBoard.getValue(n,column);
+                if(val == -1 && sudokuBoard.getPossibilities(n,column).contains(possibility)) result = false;
+            }
+            int x = (column -1) /SUBSECTION_SIZE;
+            int y = (row -1) /SUBSECTION_SIZE;
+            for (int r = y*3 +1; r<y*3+SUBSECTION_SIZE +1; r++) {
+                for (int c = x*3 +1; c < x*3+SUBSECTION_SIZE +1; c++){
+                    Integer val = sudokuBoard.getValue(r,c);
+                    if(val == -1 && sudokuBoard.getPossibilities(r,c).contains(possibility)) result = false;
+                }
+            }
+            if (result){
+                return possibility;
+            }
+        }
+        return -1;
     }
 
     private void clearPossibilities(int row, int column, SudokuBoard sudokuBoard) {
--- kodilla-sudoku/src/test/java/com/kodilla/sudoku/BoardTestSuite.java
+++ kodilla-sudoku/src/test/java/com/kodilla/sudoku/BoardTestSuite.java
@@ -7,7 +7,7 @@ public class BoardTestSuite {
     public SudokuBoard initBoard(){
         SudokuBoard board = new SudokuBoard();
         //Tu wypełnić planszę przykładowym sudoku
-        board.setValue(1,1,8);
+        /*board.setValue(1,1,8);
         board.setValue(2,3,3);
         board.setValue(2,4,6);
         board.setValue(3,2,7);
@@ -28,7 +28,20 @@ public class BoardTestSuite {
         board.setValue(8,8,1);
         board.setValue(9,2,9);
         board.setValue(9,7,4);
-        return board;
+        return board;*/
+       board.setValue(1,2,2);
+       board.setValue(1,4,4);
+       board.setValue(1,5,5);
+       board.setValue(1,6,6);
+       board.setValue(1,7,7);
+       board.setValue(1,8,8);
+       board.setValue(2,3,3);
+       board.setValue(2,1,4);
+       board.setValue(2,2,8);
+       board.setValue(3,1,5);
+       board.setValue(3,2,6);
+       board.setValue(3,3,7);
+       return board;
     }
     @Test
     public void testSudokuBoard(){
