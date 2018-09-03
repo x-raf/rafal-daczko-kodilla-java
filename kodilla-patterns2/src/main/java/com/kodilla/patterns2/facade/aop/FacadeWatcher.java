21c0599934657aed50dae4982b419bb02c526dc4
“x-raf <x-raf@wp.pl>
2018-09-03 20:08:06 +0200

    Submodule 20.3

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/aop/FacadeWatcher.java
@@ -0,0 +1,19 @@
+package com.kodilla.patterns2.facade.aop;
+
+import com.kodilla.patterns2.facade.api.OrderDto;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class FacadeWatcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FacadeWatcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..)) && args(orderDto, userId)")
+    public void logEvent(OrderDto orderDto, long userId) {
+        LOGGER.info("Logging an order by user: " + userId);
+    }
+}
