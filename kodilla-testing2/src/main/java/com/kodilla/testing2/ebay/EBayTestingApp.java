005ac6a7948bd549be61d1b1d38afb257174a71d
“x-raf <x-raf@wp.pl>
2018-09-18 23:44:53 +0200

    Submodule 23.2

--- kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -1,4 +1,18 @@
 package com.kodilla.testing2.ebay;
 
+import com.kodilla.testing2.google.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class EBayTestingApp {
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.ebay.com");
+
+        WebElement searchField = driver.findElement(By.name("_nkw"));
+        searchField.sendKeys("Laptop");
+        searchField.submit();
+    }
 }
